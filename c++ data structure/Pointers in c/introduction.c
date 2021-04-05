d866c1fba77aad0dd8edbf7da0a0bcb7ae28a816
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-04-05 21:18:03 +0600

    new

--- c++ data structure/Pointers in c/introduction.c
+++ c++ data structure/Pointers in c/introduction.c
@@ -4,4 +4,7 @@ int main() {
     int a = 5; //integer type variable
     int *p = &a; // integer pointer usually has * in it and '&a' is pointing to the location of integer variable a
     printf("%d", *p);//here this is the pointer value of p which has * in it so this will output 5
+    printf("The location of integer a is %d /n", &a);//this is the location of integer a
+    printf("The location what the pointer is pointing is %d/n", p);//this is where p pointer is pointing to
+    printf("The next pointer of p is %d", (p+1));//The next pointer which can hold an integer is going to be biger than p by 4 because integer takes 4 bytes
 }
